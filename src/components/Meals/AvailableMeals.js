afe4d7b216c8f93cac381b47693f1255c0db9753
himanshu shukla <hemi8085@gmail.com>
2024-04-04 12:03:40 +0530

    Making the Items visible

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import React from "react";
-import classes from './AvailableMeals.module.css'
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
   {
@@ -29,11 +31,22 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
-    
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <li>
+      <MealItem
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    </li>
+  ));
+
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
